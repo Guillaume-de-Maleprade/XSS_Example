ac9fbc86f335c081def3662566c6809810490c5e
unknown <g.de.maleprade@gmail.com>
2020-01-14 12:03:01 +0100

    init

---
+++ View.php
@@ -0,0 +1,17 @@
+<?php
+
+
+class View{
+    static function render($templatePath, $array){
+        echo self::getTemplate($templatePath, $array);
+    }
+
+    static function getTemplate($templatePath, $array){
+        $template = file_get_contents($_SERVER['DOCUMENT_ROOT']."/XSS_Example/templates/$templatePath");
+        foreach($array as $key=>$value){
+            $template = str_replace("{{{$key}}}", $value, $template);
+        }
+        $template = preg_replace("/{{(.*?)}}/","",$template); // get rid of ignored keywords
+        return $template;
+    }
+}
